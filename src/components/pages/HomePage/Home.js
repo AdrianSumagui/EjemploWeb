0ea306a1cad4d82721d06fd7c480df8f5428cf5b
Adrián Sumagui <adriansumagui@outlook.com>
2021-05-27 12:56:13 +0200

    Completado el archivo Pricing.js

--- src/components/pages/HomePage/Home.js
+++ src/components/pages/HomePage/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import HeroSection from '../../HeroSection';
 import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
+import HeroSection from '../../HeroSection';
+import Pricing from '../../Pricing';
+
 
 function Home() {
 
@@ -10,7 +12,9 @@ function Home() {
         <HeroSection {...homeObjOne}/>
         <HeroSection {...homeObjTwo}/>
         <HeroSection {...homeObjThree}/>
+        <Pricing/>
         <HeroSection {...homeObjFour}/>
+
 
     </>
     
