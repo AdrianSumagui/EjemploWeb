a87f1ea2825dd8bd7fd842dcee956a1f8463da2f
AdrianDEV <adriansumagui@outlook.com>
2021-06-26 11:09:26 +0200

    Revert "Pequeña corrección"
    This reverts commit 0ed968c7ebb3deb5d4a08a9a09e6777c36fff986.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import NavBar from './components/NavBar';
 import Footer from './components/pages/Footer/Footer';
 import Home from './components/pages/HomePage/Home';
 import Services from './components/pages/Services/Services';
-import Products from './components/pages/Products/Products';
-//import SignUp from './components/pages/SignUp/SignUp';
+import Products from './components/pages/Products/Productss';
+import SignUp from './components/pages/SignUp/SignUp';
 
 function App() {
   return (
@@ -16,9 +16,7 @@ function App() {
         <Route path = '/' exact component = {Home}/>
         <Route path = '/services' exact component = {Services}/>
         <Route path = '/products' exact component = {Products}/>
-
-        {/*<Route path = '/sign-up' exact component = {SignUp}/>*/}
-
+        <Route path = '/sign-up' exact component = {SignUp}/>
       </Switch>
       <Footer/>
 
